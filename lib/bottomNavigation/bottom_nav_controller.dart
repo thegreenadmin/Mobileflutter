a192d00e3b50c746d8be63ffd2a9ce74e1f90adf
ramandeep-smartdata <ramandeep.smartdata@gmail.com>
2023-04-14 18:06:05 +0530

    cart user address issue resolved

--- lib/bottomNavigation/bottom_nav_controller.dart
+++ lib/bottomNavigation/bottom_nav_controller.dart
@@ -32,7 +32,7 @@ class BottomNavController extends GetxController {
       ));
       initialRemoteMessage = null;
     }
-    selectedIndex.value = Get.arguments["currentIndex"];
+    selectedIndex.value = Get.arguments!=null ?Get.arguments["currentIndex"]??0: 0;
   }
 
   List<Widget> tabs = [
