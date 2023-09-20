8e5ab4be4940e7635383745f96e6dde2f5164310
ramandeep-smartdata <ramandeep.smartdata@gmail.com>
2023-09-20 17:57:42 +0530

    order controller issue solved

--- lib/dashboard/home/controller/store_home_main_controller.dart
+++ lib/dashboard/home/controller/store_home_main_controller.dart
@@ -1313,7 +1313,7 @@ class StoreHomeMainController extends GetxController {
             showLoading: showLoading.value)
         .then((value) async {
       isLoading.value = false;
-      debugPrint("Feature ProductList Store *******${value?.body}");
+      log("Feature ProductList Store *******${value?.body}");
       if (value?.body["status"] == ApiConstants.statusCode201 ||
           value?.body["status"] == ApiConstants.statusCode200) {
         featureProductListResponse =
